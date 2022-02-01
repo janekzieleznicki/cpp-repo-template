05f7095cb0a78ab5ce0dd7415309e010190e2393
Janek Zieleźnicki <janek.zieleznicki@gmail.com>
2022-02-01 23:45:55 +0100

    update project options

--- src/src/main.cpp
+++ src/src/main.cpp
@@ -1,19 +1,19 @@
 #include <iostream>
 
 #include "Foo.hpp"
+#include <csignal>
 #include <semaphore>
 #include <stop_token>
 #include <thread>
-#include <csignal>
 
 std::binary_semaphore semaphore{0};
 
 int main() {
-  std::signal(SIGTERM, [](int){ semaphore.release();});
-  std::signal(SIGINT, [](int){ semaphore.release();});
+  std::signal(SIGTERM, [](int) { semaphore.release(); });
+  std::signal(SIGINT, [](int) { semaphore.release(); });
   while (!semaphore.try_acquire()) {
     // sanitizer test
-     auto i = new int;
+    auto i = new int{};
     std::cout << "Address: " << i << std::endl;
   }
   return 0;
